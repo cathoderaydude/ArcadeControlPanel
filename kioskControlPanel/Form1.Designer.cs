303b5fa9b6a546d71e92aa1348d75cbfa284b97f
dthom <dthompson.wa@gmail.com>
2019-10-08 10:21:14 -0700

    Initial prototype

--- kioskControlPanel/Form1.Designer.cs
+++ kioskControlPanel/Form1.Designer.cs
@@ -29,12 +29,42 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(184, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(255, 76);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 32;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- kioskControlPanel/Form1.cs
+++ kioskControlPanel/Form1.cs
@@ -7,14 +7,120 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FTD2XX_NET;
 
 namespace kioskControlPanel
 {
     public partial class Form1 : Form
     {
+        public static FTDI ftdi;
+        public static FTDI.FT_STATUS ft_status;
+        public static UInt32 bytesWritten;
+
+        private int counter;
+
         public Form1()
         {
             InitializeComponent();
+
+            ftdi = new FTDI();
+            ft_status = FTDI.FT_STATUS.FT_OK;
+            bytesWritten = 0;
+
+            keysDown = new bool[] {false, false, false, false};
+            counter = 0;
+
+            ft_status = ftdi.OpenByIndex(0);
+            ft_status = ftdi.SetBitMode(255, 1);
+            ft_status = ftdi.SetBaudRate(300);
+            //ft_status = ftdi.SetTimeouts(5, 5);
+        }
+
+        public static bool[] keysDown;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        int x;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int count = 0;
+
+            byte[] readData = new byte[256];
+            uint bytesRead = 0;
+
+
+            ft_status = ftdi.GetRxBytesAvailable(ref bytesRead);
+            if (ft_status != FTDI.FT_STATUS.FT_OK)
+            {
+                throw new Exception("Failed to get number of bytes available to read (error " + ft_status.ToString() + ")");
+                //return;
+            }
+
+            ftdi.Read(readData, bytesRead, ref bytesRead);
+            //Console.WriteLine("Data bytes: " + bytesRead.ToString());
+            //string readDataS = System.Text.Encoding.ASCII.GetString(readData, 0, (int)bytesRead).Replace("\\", "");
+            //Console.WriteLine("Data:" + readDataS);
+            //Console.WriteLine("Data:" + readData[0].ToString());
+            //Console.Write("Data: ");
+            /*foreach (byte b in readData)
+            {
+                Console.Write(b.ToString());
+            }
+            Console.WriteLine();*/
+            String aa = "";
+            for (x=0;x<256;x++)
+            {
+                aa += readData[x].ToString();
+            }
+            //Console.WriteLine(aa);
+            
+            if ( ( ( ( (int) readData[0]) & 16) != 0) && keysDown[0] == false)
+            {
+                keysDown[0] = true;
+                SendKeys.Send("{F5}");
+                Console.WriteLine("F5");
+            }
+            else if (readData[0] != 16)
+            {
+                keysDown[0] = false;
+            }
+
+            if (((((int)readData[0]) & 64) != 0) && keysDown[1] == false)
+            {
+                keysDown[1] = true;
+                SendKeys.Send("%{F4}");
+                Console.WriteLine("Alt-F4");
+            }
+            else if (readData[0] != 64)
+            {
+                keysDown[1] = false;
+            }
+
+            if (((((int)readData[0]) & 128) != 0) && keysDown[2] == false)
+            {
+                keysDown[2] = true;
+                SendKeys.Send("{Esc}");
+                Console.WriteLine("Escape");
+            }
+            else if (readData[0] != 128)
+            {
+                keysDown[2] = false;
+            }
+
+            if (((((int)readData[0]) & 32) != 0) && keysDown[3] == false)
+            {
+                keysDown[3] = true;
+                SendKeys.Send("{5}");
+                Console.WriteLine("5");
+            }
+            else if (readData[0] != 32)
+            {
+                keysDown[3] = false;
+            }
         }
     }
 }
